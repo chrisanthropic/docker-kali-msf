5704bc53f12deb3cd35521a16ba4b7e805f949e6
Chris <ctarwater@madsecurity.com>
2015-02-17 19:04:37 -0800

    remove sudo from init.sh

--- init.sh
+++ init.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # start the database
-sudo service postgresql start
+service postgresql start
 
 # start metasploit
-sudo service metasploit start
+service metasploit start
 
 # start the metasploit shell
-sudo msfconsole
+msfconsole
